da936f03211dcc0c47e3ebea2f291aac0def8f0c
yonghoChoi2157 <youngho2157@naver.com>
2022-06-23 22:22:02 +0900

    20220623 자료구조 학습

--- studyDataStructure/src/day_20220623/Median2.java
+++ studyDataStructure/src/day_20220623/Median2.java
@@ -1,6 +1,23 @@
 package day_20220623;
 
 public class Median2 {
+	
+	static int med3(int a, int b, int c) {
+		if (a >= b) {
+			if (b >= c) 
+				return b;
+			else if(a <= c)
+				return a;
+			else
+				return c;	
+		} else if(a > c) {
+			return a;
+		} else if (b > c) {
+			return c;
+		} else {
+			return b;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
